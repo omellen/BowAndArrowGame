34bf07389ba5d515fad6fcb776fdbc10146df2a3
omellen2269 <olivia.mellen@live.com>
2021-11-23 13:32:39 -0600

    created arrow launch

--- BowAndArrowGame/Assets/ArrowScript.cs
+++ BowAndArrowGame/Assets/ArrowScript.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class ArrowScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float launchSpeed = 80f;
+
+    public GameObject Arrow;
+    public Transform ShotPoint;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Quaternion arrowRotation = new Quaternion(ShotPoint.rotation.w, 0, ShotPoint.rotation.y, ShotPoint.rotation.z);
+            GameObject createArrow = Instantiate(Arrow, ShotPoint.position, arrowRotation);
+            createArrow.GetComponent<Rigidbody>().velocity = ShotPoint.transform.up * launchSpeed;
+        }
     }
 }
